639841bdb07d8cc15ac2f9286a5bdf1a900d7d1f
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2025-02-01 07:49:08 -0300

    create input, validation and display functions

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function estaVazio(valor) {
+    if (valor == "") {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value
+
+    if (estaVazio(amigo)) {
+        alert('Digite um nome')
+    } else {
+        amigos.push(amigo)
+        document.getElementById('amigo').value = ""
+    }
+    listarAmigos()
+}
+
+function listarAmigos() {
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = amigos
+        .map(amigo => `<li>${amigo}</li>`)
+        .join('')
+}
