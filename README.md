32f93df9cc301c34e7a6a5b0afca9159562fbc60
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2025-02-02 17:45:20 -0300

    challenge done

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # ORACLE_ONE-Challenge-Amigo_secreto
+
  
 ## Amigo Secreto
-Neste desafio, você desenvolverá uma aplicação que permita aos usuários inserir nomes de amigos em uma lista para, em seguida, realizar um sorteio aleatório e determinar quem é o "amigo secreto".
+O objetivo deste desafio, é desenvolver uma aplicação que permita aos usuários inserir nomes de amigos em uma lista para, em seguida, realizar um sorteio aleatório e determinar quem é o "amigo secreto".
 
-O usuário deverá adicionar nomes por meio de um campo de texto e de um botão "Adicionar".
+O usuário pode adicionar nomes por meio de um campo de texto e de um botão "Adicionar".
 
-Os nomes inseridos serão exibidos em uma lista visível na página, e ao finalizar, um botão "Sortear Amigo" selecionará um dos nomes de forma aleatória, exibindo o resultado na tela.
+Os nomes inseridos são exibidos em uma lista visível na página e o botão "Sortear Amigo" seleciona um dos nomes de forma aleatória e exibe o resultado na tela.
 
 ## Funcionalidades:
 *Adicionar nomes:* Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
@@ -15,3 +16,21 @@ Os nomes inseridos serão exibidos em uma lista visível na página, e ao finali
 *Visualizar a lista:* Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada.
 
 *Sorteio aleatório:* Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.
+
+## Funções Principais
+
+- `estaVazio()`: Valida se um campo está vazio
+- `adicionarAmigo()`: Adiciona novo participante à lista
+- `listarAmigos()`: Exibe lista atualizada de participantes
+- `sortearAmigo()`: Realiza sorteio aleatório de um participante
+- `removerAmigo()`: Remove participante específico da lista
+
+## Principais Características
+
+1. Sorteio automático e aleatório
+2. Os nomes sorteados são retirados da lista
+
+## Possíveis Melhorias
+
+1. Persistência de dados (salvamento local/banco de dados)
+2. Validação de nomes duplicados
--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// lista de amigos
 let amigos = []
 
+/**
+ * Verifica se o valor fornecido é uma string vazia.
+ * @param {string} valor - O valor a ser verificado.
+ * @returns {boolean} Retorna true se o valor for uma string vazia, caso contrário false.
+ */
 function estaVazio(valor) {
     if (valor == "") {
         return true
@@ -8,6 +14,12 @@ function estaVazio(valor) {
     }
 }
 
+/**
+ * Adiciona um novo amigo à lista de amigos.
+ * Verifica se o campo de entrada está vazio e exibe um alerta se necessário.
+ * Caso contrário, adiciona o nome à lista e limpa o campo de entrada.
+ * Atualiza a exibição da lista de amigos.
+ */
 function adicionarAmigo() {
     let amigo = document.getElementById('amigo').value
 
@@ -20,9 +32,41 @@ function adicionarAmigo() {
     listarAmigos()
 }
 
+/**
+ * Atualiza a lista de amigos com os elementos da lista amigos.
+ * Chama innerHTML para recriar a lista com os elementos da lista amigos.
+ */
 function listarAmigos() {
     let lista = document.getElementById('listaAmigos')
     lista.innerHTML = amigos
         .map(amigo => `<li>${amigo}</li>`)
         .join('')
+}
+
+/**
+ * Sorteia aleatoriamente um amigo da lista de amigos e exibe o resultado.
+ * Se a lista estiver vazia, exibe um alerta solicitando a adição de amigos.
+ * Após o sorteio, remove o amigo sorteado da lista e atualiza a exibição da lista.
+ */
+function sortearAmigo() {
+    resultado = document.getElementById('resultado')
+    if (amigos.length == 0) {
+        alert('Adicione amigos antes de sortear')
+    } else {
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)]
+        resultado.innerHTML = `O amigo sorteado é: ${amigoSorteado}`
+        removerAmigo(amigoSorteado)
+        listarAmigos()
+    }
+}
+
+/**
+ * Remove um amigo da lista de amigos.
+ * @param {string} amigoParaRemover - Nome do amigo a ser removido.
+ */
+function removerAmigo(amigoParaRemover) {
+    const index = amigos.indexOf(amigoParaRemover)
+    if (index > -1) {
+        amigos.splice(index, 1)
+    }
 }
